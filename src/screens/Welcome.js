4d0fb45b2ba7126d9ddb979bf3e2aa948b7999f5
n1ktrnv <n1ktrnv@gmail.com>
2022-03-18 13:56:16 +0300

    Add project

---
+++ src/screens/Welcome.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+export default class Welcome extends React.Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    height: '100%',
+                    padding: 40,
+                    paddingTop: 50,
+                    backgroundColor: 'white'
+                }}>
+                <View
+                    style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginBottom: 30,
+                    }}>
+                    <Image
+                        source={require('../images/image.jpg')}
+                        style={{
+                            width: 250,
+                            height: 80,
+                        }}
+                    />
+                </View>
+                <Text
+                    style={{
+                        fontSize: 30,
+                        fontWeight: 'bold',
+                        alignSelf: 'center',
+                    }}>
+                    Добро пожаловать!
+                </Text>
+                <Text
+                    style={{
+                        textAlign: 'center',
+                        color: 'grey',
+                        marginBottom: 50,
+                    }}>
+                    Вы вошли в систему как
+                    {this.props.route.params.email}
+                </Text>
+            </View>
+        );
+    }
+}
